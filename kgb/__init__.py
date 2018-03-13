0f003e226d1b435d6cae96c5bbb8f777cdc1bd02
Christian Hammond <christian@beanbaginc.com>
2018-03-12 23:38:27 -0700

    Release kgb 2.0.1.

--- kgb/__init__.py
+++ kgb/__init__.py
@@ -10,7 +10,7 @@ from kgb.contextmanagers import spy_on
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (2, 0, 1, 'alpha', 0, False)
+VERSION = (2, 0, 1, 'final', 0, True)
 
 
 def get_version_string():

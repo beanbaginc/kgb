4c7f4361a8050e5426cb23e4a84ee64df25a6c12
Christian Hammond <christian@beanbaginc.com>
2020-09-03 23:57:34 -0700

    Bump to 6.0.1 dev.

--- kgb/__init__.py
+++ kgb/__init__.py
@@ -11,7 +11,7 @@ from kgb.ops import SpyOpMatchAny, SpyOpMatchInOrder, SpyOpRaise, SpyOpReturn
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (6, 0, 0, 'final', 0, True)
+VERSION = (6, 0, 1, 'alpha', 0, False)
 
 
 def get_version_string():
